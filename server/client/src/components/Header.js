8b6c1a626862a0e282bb8ee3410dcf8b4b10d225
Jesse Calton <jessecalton@gmail.com>
2020-11-17 21:13:20 -0800

    imports materialize in index.js

---
+++ server/client/src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <nav>
+        <div className='nav-wrapper'>
+          <a className='left brand-logo'>Emaily</a>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Header;
